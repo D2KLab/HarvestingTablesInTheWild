8112d40927466a00839ece7c33631a54eadf2aa3
Jack Henschel <jackdev@mailbox.org>
2020-10-23 11:41:33 +0200

    Implement fetching of HTML pages with requests library

--- crawlinator/crawlinator.py
+++ crawlinator/crawlinator.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
+
+import requests
 
 def main():
-    parser = argparse.ArgumentParser(prog=__name__, description='TODO')
+    parser = argparse.ArgumentParser(prog='crawlinator.py',
+                                     description='Fetches and extracts table from HTML pages')
     parser.add_argument('url', nargs='*', action='append')
     args = parser.parse_args()
-    for u in args.url[0]:
-        pipeline(u)
+    if len(args.url[0]) == 0:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    for url in args.url[0]:
+        pipeline(url)
 
 def pipeline(url: str):
-    print(fetch(url))
+    html = fetch(url)
+    table = extract_table(html)
+    print(table)
 
 def fetch(url: str) -> str:
-    return 'TODO: fetch ' + url
+    # by default, requests will follow redirects
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise ValueError('Failed to fetch', url, ', status code:', resp.status_code)
+
+    return resp.text
+
+def extract_table(html: str) -> str:
+    _ = html
+    return 'TODO: extract table'
 
 if __name__ == "__main__":
     main()
