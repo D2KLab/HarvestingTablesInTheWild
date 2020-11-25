2a48da6dacc33bc8a3fdccab2ee3af53d5c85fbb
Jack Henschel <jackdev@mailbox.org>
2020-11-25 17:09:11 +0100

    Tune scrapy settings for broad crawling
    as per: https://docs.scrapy.org/en/latest/topics/broad-crawls.html

--- core/settings.py
+++ core/settings.py
@@ -20,22 +20,28 @@ LOG_LEVEL = 'ERROR'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+# Use a priority queue that works well for multi-domain crawls
+SCHEDULER_PRIORITY_QUEUE = 'scrapy.pqueues.DownloaderAwarePriorityQueue'
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 100
 
 # Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
-# CONCURRENT_REQUESTS_PER_IP = 16
+DOWNLOAD_DELAY = 1
+DOWNLOAD_TIMEOUT = 10
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
 
 # Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-# TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
+
+# Increase threadpool size for faster DNS resolution
+REACTOR_THREADPOOL_MAXSIZE = 32
+
+# Disable retries for faster crawls
+RETRY_ENABLED = False
 
 # Override the default request headers:
 # DEFAULT_REQUEST_HEADERS = {
