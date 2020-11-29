a4db81217f94c646cd2bd17b0627c080a1e610d7
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2020-11-29 15:08:21 +0100

    Use log-level INFO

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BOT_NAME = 'core'
 SPIDER_MODULES = ['core.spiders']
 NEWSPIDER_MODULE = 'core.spiders'
 
-LOG_LEVEL = 'ERROR'
+LOG_LEVEL = 'INFO'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = 'core (+http://www.yourdomain.com)'
