1c741adabb7a0f13c20128b0ade20bb1a449fdc4
Jack Henschel <jackdev@mailbox.org>
2020-12-08 10:16:15 +0100

    DRY up spiders tests

--- core/spiders/web_test.py
+++ core/spiders/web_test.py
@@ -7,9 +7,7 @@ from core.spiders.fixtures.w3_schools import w3_schools
 from core.spiders.fixtures.wiki_nhl import wiki_nhl
 
 
-def test_parse_w3_html_tables():
-    url, html, expected_output = w3_schools()
-
+def run_test(url, html, expected_output):
     resp = TextResponse(url, body=html)
     os.environ['URL_STRING'] = url
     got = list(TableParserSpider().parse(resp))
@@ -20,21 +18,14 @@ def test_parse_w3_html_tables():
         t.timestamp = 'FAKE'
 
     # test each item individually (improves readability of the diff)
+    assert len(got) == len(expected_output)
     for i, _ in enumerate(got):
         assert got[i] == expected_output[i]
+
+
+def test_parse_w3_html_tables():
+    run_test(*w3_schools())
+
 
 def test_parse_wikipedia_nhl():
-    url, html, expected_output = wiki_nhl()
-
-    resp = TextResponse(url, body=html)
-    os.environ['URL_STRING'] = url
-    got = list(TableParserSpider().parse(resp))
-    del os.environ['URL_STRING']
-
-    # patch timestamp field
-    for t in got:
-        t.timestamp = 'FAKE'
-
-    # test each item individually (improves readability of the diff)
-    for i, _ in enumerate(got):
-        assert got[i] == expected_output[i]
+    run_test(*wiki_nhl())
