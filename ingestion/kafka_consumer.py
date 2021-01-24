95b0d16f628f1962407fefd7c8fa48b6f01bead8
Jack Henschel <jackdev@mailbox.org>
2021-01-24 09:50:24 +0100

    Disable preprocessing during ingestion
    As the annotation API is rather buggy at the moment and does not
    support "parallel" invocations.
    If it gets fixed, we will perform this task in the postprocessing step

--- ingestion/kafka_consumer.py
+++ ingestion/kafka_consumer.py
@@ -1,7 +1,7 @@
 import os
 import json
 import logging
-from postprocessing.annotation import TableAnnotationAPIClient
+# from postprocessing.annotation import TableAnnotationAPIClient
 from ingestion.collection import ArangoTableCollector
 from kafka import KafkaConsumer
 
@@ -32,12 +32,12 @@ class Consumer:
         self.topic_name = os.environ.get('KAFKA_TOPIC')
         if not self.topic_name:
             raise Exception('KAFKA_TOPIC not defined')
-        
+
         self.database_collector = ArangoTableCollector()
         self.consumer = KafkaConsumer(bootstrap_servers = bootstrap_servers,
                         auto_offset_reset = self.__OFFSET_LISTENING_POLICY,
                         max_poll_records = self.__MAX_POLL_RECORDS)
-        self.annotation_client = TableAnnotationAPIClient()
+        # self.annotation_client = TableAnnotationAPIClient()
 
         rootLogger.info('Connected to Kafka brokers at %s', bootstrap_servers)
 
@@ -59,22 +59,24 @@ class Consumer:
             rootLogger.error('Unable to parse the json incoming message with offset %s', message.offset)
             return
 
-        try:
-            preprocess_additions = self.annotation_client.preprocess(payload)
+        # Preprocessing during Ingestion disabled for now,
+        # due to buggy annotation API
+        # try:
+        #     preprocess_additions = self.annotation_client.preprocess(payload)
 
-            if 'task_id' not in preprocess_additions:
-                raise Exception('Unexpected preprocess output', preprocess_additions)
+        #     if 'task_id' not in preprocess_additions:
+        #         raise Exception('Unexpected preprocess output', preprocess_additions)
 
-            payload['preprocessing'] = preprocess_additions
-        except Exception as e:
-            rootLogger.exception('Failed to preprocess table: %s', e)
+        #     payload['preprocessing'] = preprocess_additions
+        # except Exception as e:
+        #     rootLogger.exception('Failed to preprocess table: %s', e)
 
         try:
             # Insert into database
             self.database_collector.append(payload)
             rootLogger.info('Succesfully inserted incoming message with offset %s on topic %s into the database', message.offset, message.topic)
         except Exception as e: # gracefully catch all the exceptions and continue message ingestion
-            rootLogger.exception('Unable to process message due to follwing error: %s', e)  
+            rootLogger.exception('Unable to process message due to follwing error: %s', e)
 
 
 def main():
